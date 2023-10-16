7b0f0d140ce5d8806017e6a690ea4737140e9184
Jonathan <emeraldusk@gmail.com>
2023-10-16 18:26:06 -0300

    feat(config): params are now configurable

--- bot/src/config/index.js
+++ bot/src/config/index.js
@@ -1,6 +1,6 @@
 export const config = {
     token: process.env.DISCORD_TOKEN,
     channelId: process.env.DISCORD_CHANNEL_ID,
-    allowedFormarts: ['application/pdf'],
-    booksTable: 'books'
+    allowedFormarts: process.env.ALLOWED_FORMATS.split(', '),
+    booksTable: process.env.BOOKS_TABLE,
 }
