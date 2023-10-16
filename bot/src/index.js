bea41690dbb13fbb0133848a4e5b2cd651da9329
Jonathan <emeraldusk@gmail.com>
2023-10-16 18:02:35 -0300

    chore: proper use of config

--- bot/src/index.js
+++ bot/src/index.js
@@ -1,5 +1,34 @@
+/* libritos-bot -- A Discord bot to save books in a database
+ *
+ * -----------------------------------------------------------------------
+ *
+ * Author: Jonathan Dyallo
+ * GNU General Public License v3.0
+ *
+ * Copyright (c) 2023 Jonathan <contacto at jonathan dot com dot ar>
+ * 
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 import { Client, GatewayIntentBits } from 'discord.js';
-import { config } from './config/index.js';
 import { db } from './config/database.js';
 import {
     isBot,
@@ -8,9 +37,29 @@ import {
     fileExists, moreThan20MB
 } from './utils/validations.js';
 
+/**
+ * @example
+    import { LibraryBot } from 'libritos-bot';
+    import { config } from './src/config/index.js';
+
+    const bot = new LibraryBot({
+        config
+    });
+    bot.start();
+    @description This is the main class of the bot, it is responsible for starting the bot and listening to the events
+    @param {Object} options - Options for the bot
+    @param {Object} options.config - Configuration for the bot
+    @param {string} options.config.token - Discord bot token
+    @param {string} options.config.channelId - Discord channel id where the bot will be listening
+    @param {string} options.config.booksTable - Name of the table where the books will be saved
+    @param {string[]} options.config.allowedFormarts - Allowed file formats
+    @returns {void}
+ */
+
 export class LibraryBot {
     constructor(options = {}) {
         this._options = options;
+        this._config = options.config;
         this._client = new Client({
             intents: [
                 GatewayIntentBits.Guilds,
@@ -19,7 +68,7 @@ export class LibraryBot {
             ],
         });
 
-        this._client.login(config.token);
+        this._client.login(this._config.token);
         this._client.on('ready', this._onReady.bind(this));
         this._client.on('messageCreate', this._onMessage.bind(this));
     }
@@ -34,7 +83,7 @@ export class LibraryBot {
 
     async _onMessage(message) {
         if (isBot(message)) return;
-        if (isNotSelectedChannel(message, config.channelId)) return;
+        if (isNotSelectedChannel(message, this._config.channelId)) return;
 
         const { name, url, contentType } = message.attachments.first() || {};
 
@@ -49,7 +98,7 @@ export class LibraryBot {
         }
 
 
-        if (!config.allowedFormarts.includes(contentType)) {
+        if (!this._config.allowedFormarts.includes(contentType)) {
             await message.reply('Please send a valid file');
             return;
         }
@@ -60,7 +109,7 @@ export class LibraryBot {
         }
 
         const query = await db
-            .insertInto(config.booksTable)
+            .insertInto(this._config.booksTable)
             .values({
                 name,
                 url,
