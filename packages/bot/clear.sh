678063de3c527a0520f48062b8852d9b0b544dc3
Jonathan <emeraldusk@gmail.com>
2023-10-21 22:26:52 -0300

    trying to refactor how the app builds

--- packages/bot/clear.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# File to modify = var getZlibSync
-# Route = node_modules/@discordjs/ws/dist/index.js
-
-sed -i 's/var getZlibSync = lazy2(async () => import("zlib-sync").then((mod) => mod.default).catch(() => null));;/var getZlibSync = null;/g' node_modules/@discordjs/ws/dist/index.js
